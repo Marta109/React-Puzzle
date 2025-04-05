76558de358362417bc2cf1228f37cead6e9a0538
Marta109 <marta.hayrapetyan.a@gmail.com>
2025-04-06 00:14:41 +0300

    feat: add puzzle move handler

--- src/components/game-board-item-word/game-board-item-word.jsx
+++ src/components/game-board-item-word/game-board-item-word.jsx
@@ -2,11 +2,30 @@ import "./game-board-item-word.css";
 
 const GameBoardItemWord = ({ word }) => {
   const wordArr = word.split(" ");
+
+  const handleClick = (e) => {
+    const spans = e.currentTarget.querySelectorAll("span");
+    const emptyPuzzleItem = document.querySelector(
+      ".puzzleItem:not(:has(span))"
+    );
+
+    if (emptyPuzzleItem && spans.length > 0) {
+      spans.forEach((span) => {
+        const clone = span.cloneNode(true);
+        emptyPuzzleItem.appendChild(clone);
+        span.remove();
+      });
+    }
+  };
+
   return (
     <>
-      {wordArr.map(( index) => (
-        <div key={index} className="gameBoardItemWord draggable">
-        </div>
+      {wordArr.map((word, index) => (
+        <div
+          key={index}
+          className="gameBoardItemWord draggable"
+          onClick={handleClick}
+        ></div>
       ))}
     </>
   );
