23ff158b94b4aa6eac45e53238dec86af7bf9462
Marta109 <marta.hayrapetyan.a@gmail.com>
2025-04-06 00:09:52 +0300

    feat: add puzzle move handler

--- src/components/puzzle-board-item/puzzle-board-item.jsx
+++ src/components/puzzle-board-item/puzzle-board-item.jsx
@@ -2,11 +2,29 @@ import "./puzzle-board-item.css";
 
 const PuzzleBoardItem = ({ word, index, itemIndex, stringArrLength }) => {
   const bothSide = itemIndex !== 0 && itemIndex !== stringArrLength;
+
+  const handleClick = (e) => {
+    const spans = e.currentTarget.querySelectorAll("span");
+    const emptyCell = document.querySelector(".gameBoardItemWord:not(:has(*))");
+
+    if (emptyCell && spans.length > 0) {
+      emptyCell.innerHTML = "";
+
+
+      spans.forEach((span) => {
+        const clone = span.cloneNode(true);
+        emptyCell.appendChild(clone);
+        span.remove(); 
+      });
+    }
+  };
+
   return (
     <div
       className="puzzleItem draggable"
       draggable="true"
       id={`puzzleItem_${index}`}
+      onClick={handleClick}
     >
       <span className="text">{word}</span>
       <span
